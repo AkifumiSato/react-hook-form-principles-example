7c60448fb556497b95404caaf67fb556fa8ac4ad
佐藤昭文 <akfm.sato@gmail.com>
2023-02-18 21:32:15 +0900

    style reset, prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@unocss/reset/eric-meyer.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
