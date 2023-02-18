3c127e668d9acd36c8189ab0a4f53348e64750a9
佐藤昭文 <akfm.sato@gmail.com>
2023-02-18 22:54:10 +0900

    add react-hook-form

--- src/components/CreditModal.tsx
+++ src/components/CreditModal.tsx
@@ -1,12 +1,15 @@
 import { Input, Text, Button, Modal, Row } from "@nextui-org/react";
+import { useForm } from "react-hook-form";
 
 type Props = {
   closeHandler: () => void;
+  onSubmit: (data: { credit: string }) => void;
 };
 
-export default function CreditModal({ closeHandler }: Props) {
+export default function CreditModal({ closeHandler, onSubmit }: Props) {
+  const { handleSubmit, register } = useForm<{ credit: string }>();
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Modal.Header>
         <Text id="modal-title" size={15}>
           クレジットカード番号を入力してください
@@ -20,13 +23,14 @@ export default function CreditModal({ closeHandler }: Props) {
           color="primary"
           size="lg"
           placeholder="xxxx-yyyy-zzzz"
+          {...register("credit")}
         />
       </Modal.Body>
       <Modal.Footer>
-        <Button auto onPress={closeHandler}>
+        <Button auto onPress={closeHandler} type="submit">
           保存
         </Button>
       </Modal.Footer>
-    </>
+    </form>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,14 @@ import Head from "next/head";
 import { Input, Text, Button, Modal } from "@nextui-org/react";
 import styles from "@/styles/Home.module.css";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const { register, handleSubmit, setValue, watch } = useForm<{
+    name: string;
+    credit: string;
+  }>();
+  const credit = watch("credit");
   const [visible, setVisible] = useState(false);
   const closeHandler = () => setVisible(false);
 
@@ -19,33 +25,49 @@ export default function Home() {
           <Text h1 size={30} weight="bold">
             サンプルForm
           </Text>
-          <form>
+          <form
+            onSubmit={handleSubmit((data) => {
+              console.log("submit: ", data);
+            })}
+          >
             <div className={styles.formInner}>
               <dl className={styles.formItem}>
                 <dt>名前</dt>
                 <dd>
-                  <Input clearable initialValue="田中太郎" width="200px" />
+                  <Input
+                    clearable
+                    initialValue="田中太郎"
+                    width="200px"
+                    {...register("name")}
+                  />
                 </dd>
               </dl>
               <dl className={styles.formItem}>
                 <dt>クレジットカード</dt>
                 <dd className={styles.formItemContents}>
-                  <Text>none</Text>
-                  <Button onClick={() => setVisible(true)} auto>
+                  <Text>{credit ?? "none"}</Text>
+                  <Button onClick={() => setVisible(true)} auto flat>
                     変更
                   </Button>
-                  <Modal
-                    closeButton
-                    aria-labelledby="modal-title"
-                    open={visible}
-                    onClose={closeHandler}
-                  >
-                    <CreditModal closeHandler={closeHandler} />
-                  </Modal>
                 </dd>
               </dl>
+              <Button type="submit">submit</Button>
             </div>
           </form>
+          <Modal
+            closeButton
+            aria-labelledby="modal-title"
+            open={visible}
+            onClose={closeHandler}
+          >
+            <CreditModal
+              closeHandler={closeHandler}
+              onSubmit={({ credit }) => {
+                console.log("CreditModal submit");
+                setValue("credit", credit);
+              }}
+            />
+          </Modal>
         </div>
       </main>
     </>
